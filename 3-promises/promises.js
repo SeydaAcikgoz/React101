3ddbca3a5c5c3e1851b1cee5c32294cf5b4b072a
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2024-03-01 16:57:32 +0300

    added async-await function

--- 3-promises/promises.js
+++ 3-promises/promises.js
@@ -24,3 +24,36 @@ const getComments2 = (number) => {
 
 getComments2(2).then((data) => console.log(data)).catch((e) => console.log(e));
 
+import axios from "axios";
+
+const getUsers = () => {
+    return new Promise(async (resolve, reject) => {
+
+            const { data } = await axios("https://jsonplaceholder.typicode.com/users");
+        resolve(data);
+    });
+};
+
+
+const getPost = (post_id) => {
+    return new Promise(async (resolve, reject) => {
+
+        const { data } = await axios("https://jsonplaceholder.typicode.com/posts/"+post_id);
+        resolve(data);
+    });
+};
+
+(async () => {
+    await getUsers().then((data) => console.log(data)).catch((e) => console.log(e));
+
+    await getPost(1).then((data) => console.log(data)).catch((e) => console.log(e));
+})();
+
+(async () => {
+    const userr = await getUsers();
+    const postt = await getPost(2);
+
+    console.log(userr);
+    console.log(postt);
+
+})();//yukarıdaki gibi de çağrılabilir bu şekilde de çağrılabilir
