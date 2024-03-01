205bd5684fb809d3fc3fca205193f45e5fce710c
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2024-03-01 17:35:32 +0300

    added array functions

---
+++ 1-temel_bilgiler/arrayFunctions.js
@@ -0,0 +1,40 @@
+const users = ["Mehmet", "Ahmet", "Murat"];
+
+//push:dizinin sonuna eleman ekler
+users.push("Ali");
+console.log(users);
+
+//map:dizi içinde dolaşmayı sağlar
+users.map((item) => {
+    console.log(item)
+});
+
+const persons = [
+    {
+        name: "Mehmet",
+        age:20,
+    },
+    {
+        name: "Mehmet",
+        age: 30,
+    },
+    {
+        name: "Ali",
+        age: 30,
+    },
+    {
+        name: "Ayşe",
+        age: 10,
+    }
+];
+persons.map((item) => {
+    console.log(item.name)
+});//objeler de bu şekilde kullanılabilir
+
+//find:dizi içinden istenilen elemanı bulmayı sağlar.
+const result = persons.find((item) => item.name === "Mehmet")
+console.log(result);
+const result2 = persons.find((item) => item.name === "Mehmete")
+console.log(result2);
+const result3 = persons.find((item) => item.name === "Mehmet" && item.age >20)
+console.log(result3);
