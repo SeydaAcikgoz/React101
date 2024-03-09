c9d8585cff0292f48bd5f51cb9495d4a367890a2
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2024-03-09 22:36:39 +0300

    added async await function
    [https://www.patika.dev/](Patika.dev)

---
+++ 2-callback/app.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+
+export async function getData(user_id) {
+    const { data: user } = await (
+        axios("https://jsonplaceholder.typicode.com/users/"+user_id)
+    );
+
+    const { data: post } = await (
+        axios("https://jsonplaceholder.typicode.com/posts/" + user_id)
+    );
+
+    console.log("User", user);
+    console.log("Post", post);
+}
+
