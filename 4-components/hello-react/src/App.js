25a6d5aa697034e6609099f91fcfd524be52e873
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2024-03-10 21:49:39 +0300

    Add files via upload
    added componenets

---
+++ 4-components/hello-react/src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import Header from './components/Header';
+import React from "react";
+
+const name = "Şeyda";
+const surname = "Açıkgöz";
+const isLoggedIn = false;
+
+function App() {
+  // return React.createElement("div",null,"Hello"); //JSX kullanmadan da yapılabilir.fakat div içinde birden fazla element olduğunda kod gereksiz uzayacağı için JSX kullanıyoruz.
+  
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Header></Header>
+      <Header />
+      <p className='xyz'>lorem ipsum</p>
+      <label htmlFor='myinput'>
+        Name
+        <input id='myinput'/>
+      </label>
+      <h1>{name}{surname}</h1>{/*componentlerde değişken render etme */}
+      <h1>{`Ben ${name} ${surname}`}</h1>
+
+      <h1>{isLoggedIn && `Ben ${name} ${surname}`}</h1>
+      {!isLoggedIn && "Giriş yapmadınız."}{/*koşullu render işlemi */}
+      
+      <h1>
+        {isLoggedIn ? `Ben ${name} ${surname}` : "Giriş yapmadınız."}{/*koşullu render işlemi böyle de yazılabilir */}
+      </h1>
+    </div>
+    //class ifadesi js ye özgü olduğu için className kullanılmalı
+    //for ifadesi js ye özgü olduğu için htmlFor kullanılmalı
+
+    //componentlerin dışında kapsayıcı element(<div> </div> ya da fragment <></>(<React.Fragment></React.Fragment>)) kullanılmalı
+    // <React.Fragment>
+    //   <h1>Hello World</h1>
+    //   <Header></Header>
+    //   <Header />
+    // </React.Fragment>
+  );
+}
+
+export default App;
---
+++ 4-components/hello-react/src/components/Header.js
@@ -0,0 +1,6 @@
+function Header(){
+    return <div>
+        Merhaba bu Header bileşeni
+    </div>
+}//component baş harfi büyük olmalı.Çünkü küçük olursa html etiketi gibi yorumlanır
+export default Header;
