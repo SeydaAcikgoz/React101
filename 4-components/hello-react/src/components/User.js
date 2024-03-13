a0fcc692d9a0aba529e34fa0ac17148b4b35df10
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2024-03-13 22:06:42 +0300

    update User

--- 4-components/hello-react/src/components/User.js
+++ 4-components/hello-react/src/components/User.js
@@ -1,11 +1,15 @@
-function User({ name, surname,age, isLoggedIn }) {
+function User({ name, surname, age, isLoggedIn, friends }) {
     return (
-        <h1>
-            {
-                isLoggedIn ? `${name} ${surname} (${age})`
-                : "Giriş yapmadınız."
-            }
-        </h1>
+        <>
+            <h1>{isLoggedIn ? `${name} ${surname} (${age})`
+                : "Giriş yapmadınız."}
+            </h1>
+            {friends.map((friend) => (
+                <div key={friend.id}>
+                    {friend.id}-{friend.name}
+                </div> //eğer burada extra işlemler yapılacaksa div return ile döndürülmeli
+            ))} 
+        </>
     )
 }
 
