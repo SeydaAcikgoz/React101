e553addc5220fc9aa1265cd59c81117b2c229a16
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2024-03-24 15:15:03 +0300

    added counter

---
+++ 5-states/state/src/Components/Counter.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useState } from "react";
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    const increase = () =>{
+        setCount(count + 1)
+    }
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={() => setCount(count - 1)}>Decrease</button>
+            <button onClick={increase}>Increase</button>
+        </div>
+    )
+}
+//eğer set fonksiyonları uzunsa dışarıda ayrıca tanımlanabilir
+export default Counter
