45cdc172d78b6c838957aac13ff8837676b7ccdd
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2024-03-24 14:36:24 +0300

    added create to state

--- 5-states/state/src/App.js
+++ 5-states/state/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [name, setName] = useState("Mehmet")
+  const [age, setAge] = useState(29)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Merhaba {name}</h1>
+      <h2> {age}</h2>
+
+      <button onClick={() => setName("Ahmet")}>Change name</button>
+      <button onClick={() => setAge(25)}>Change age</button>
+
     </div>
   );
 }
