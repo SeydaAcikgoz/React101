dd1161a2f749a0d79222e28f910e130fc971f187
SeydaAcikgoz <123414515+SeydaAcikgoz@users.noreply.github.com>
2024-03-24 15:06:17 +0300

    added object state

--- 5-states/state/src/App.js
+++ 5-states/state/src/App.js
@@ -3,9 +3,10 @@ import { useState } from "react";
 import './App.css';
 
 function App() {
-  const [name, setName] = useState("Mehmet")
-  const [age, setAge] = useState(29)
-  const [friends, setFriends] = useState(["Ahmet", "Murat"])
+  const [name, setName] = useState("Mehmet");
+  const [age, setAge] = useState(29);
+  const [friends, setFriends] = useState(["Ahmet", "Murat"]);
+  const [address, setAddress] = useState({ title: "İstanbul", zip: 29344 });
 
   return (
     <div className="App">
@@ -22,12 +23,21 @@ function App() {
         <div key="index">{friend}</div>
       ))}
 
-      <button onClick={() => setFriends([...friends,"Ayşe"])}>Add new friend</button>
+      <button onClick={() => setFriends([...friends, "Ayşe"])}>Add new friend</button>
 
+      <hr></hr>
+
+      <h2>Address</h2>
+      <div>
+        {address.title} {address.zip}
+      </div>
+      <button onClick={() => setAddress({ ...address, title: "Ankara" })}>Change Address</button>
     </div>
   );
 }
 // eğer array olarak tanımlanmış state değitirilecek ise o zaman yeni değer yine array olamlı
-
+//array state lerde [...friends,"Ayşe"] öncekilere Ayşe ekle demektir
+//state lerde set işlemi yapışırken dönen değer tipi ilk tanımlanan değer tipi ile aynı olamlı
+//object state lerde set yapılırken {...address, title: "Ankara"} böyle yapılırsa zip değişkeni önceki değerde kalır title değişkeni yeni değeri alır
 
 export default App;
